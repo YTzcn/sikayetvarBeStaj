72bcea0800d6cfca0d03e9ffa8bab9947480fb56
Yahya Tezcan <yahyatezcan.yahya@gmail.com>
2026-07-20 15:40:46 +0300

    initial Spring Boot commit

---
+++ src/main/java/com/sikayetvar/beStaj/BeStajApplication.java
@@ -0,0 +1,13 @@
+package com.sikayetvar.beStaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeStajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeStajApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sikayetvar/beStaj/BeStajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sikayetvar.beStaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeStajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
